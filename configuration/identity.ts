aa8623e4a2a54c5ede54a5d1317dbf4e6bd1b04e
Sylvie Bon-Harper <scbonharper@gmail.com>
2025-04-09 08:37:58 -0400

    Update identity.ts
    fixed name

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Sylvie`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
-export const AI_NAME: string = `Constance`;
+export const AI_NAME: string = `Connie`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
