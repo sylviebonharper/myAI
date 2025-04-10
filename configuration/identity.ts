ddcb620d8bc69fde2b5cd331f1239edc3033030c
Sylvie Bon-Harper <scbonharper@gmail.com>
2025-04-09 21:37:39 -0400

    Update identity.ts
    Added tone, owner description, and role

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Sylvie`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_DESCRIPTION: string = `Student`;
 
 export const AI_NAME: string = `Connie`;
-export const AI_TONE: string = `[AI TONE]`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_TONE: string = `Helpful`;
+export const AI_ROLE: string = `Helper`;
